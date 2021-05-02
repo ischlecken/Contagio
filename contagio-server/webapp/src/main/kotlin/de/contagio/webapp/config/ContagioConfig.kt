1d157742bb0ce5143adab9894a43645aeddd0e90
Stefan Thomas <stefan.thomas@goyellow.de>
2021-05-02 13:45:12 +0200

    update verify page

--- contagio-server/webapp/src/main/kotlin/de/contagio/webapp/config/ContagioConfig.kt
+++ contagio-server/webapp/src/main/kotlin/de/contagio/webapp/config/ContagioConfig.kt
@@ -1,28 +1,33 @@
 package de.contagio.webapp.config
 
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import com.fasterxml.jackson.datatype.jsr310.ser.InstantSerializer
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
-import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
 
+
+private var logger = LoggerFactory.getLogger(ContagioConfig::class.java)
+
+private const val dateTimeFormat = "yyyy-MM-dd'T'HH:mm:ss"
 
 @Configuration
 @ConfigurationPropertiesScan("de.contagio.webapp.model.properties")
 open class ContagioConfig {
 
-    private val dateFormat = "yyyy-MM-dd"
-    private val dateTimeFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
-
     @Bean
-    open fun jsonCustomizer(): Jackson2ObjectMapperBuilderCustomizer? {
+    open fun jsonCustomizer(): Jackson2ObjectMapperBuilderCustomizer {
+        logger.debug("jsonCustomizer()")
+
+        val df = DateTimeFormatterBuilder().appendInstant(0).toFormatter()
+
         return Jackson2ObjectMapperBuilderCustomizer { builder: Jackson2ObjectMapperBuilder ->
             builder.simpleDateFormat(dateTimeFormat)
-            builder.serializers(LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)))
-            builder.serializers(LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)))
+
+            builder.serializers(object : InstantSerializer(InstantSerializer.INSTANCE, false, df) {})
         }
     }
 }
